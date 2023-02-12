923561f56418d644e6a268aca5a6ef2d6d47f380
gaon-park <do.judo1224@gmail.com>
2023-02-13 00:35:57 +0900

    init commit

---
+++ next.config.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
+/** @type {import('next').NextConfig} */
+
+// Remove this if you're not using Fullcalendar features
+const withTM = require('next-transpile-modules')([
+  '@fullcalendar/common',
+  '@fullcalendar/react',
+  '@fullcalendar/daygrid',
+  '@fullcalendar/list',
+  '@fullcalendar/timegrid'
+])
+
+module.exports = withTM({
+  trailingSlash: true,
+  reactStrictMode: false,
+  experimental: {
+    esmExternals: false
+  },
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
+
+    return config
+  }
+})
+
+const nextConfig = {
+  reactStrictMode: false,
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
