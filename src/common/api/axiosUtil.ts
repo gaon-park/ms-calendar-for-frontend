bae72171caa32e2ca34a27f7f21fdda24575a979
gaon-park <do.judo1224@gmail.com>
2023-02-16 16:17:22 +0900

    fix: reissue logic

--- src/common/api/axiosUtil.ts
+++ src/common/api/axiosUtil.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import * as qs from "qs";
 import { AuthCookie } from "../cookie/cookies";
+import isJwtTokenExpired from "jwt-check-expiry";
+import { MS_BACKEND_API_PATH } from "src/constants/msbackend";
 
 export const MsBackendAxios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL ?? "",
@@ -14,3 +16,34 @@ export const MsBackendAxios = axios.create({
 });
 
 MsBackendAxios.defaults.headers.common["X-OS-TYPE"] = "WEB";
+MsBackendAxios.interceptors.request.use(
+  async function (config) {
+    if (!isTokenExpired()) {
+      console.log('expired token! run reissue logic')
+      await axios.post(
+        MS_BACKEND_API_PATH.POST_REISSUE_TOKEN,
+        {},
+        {
+          baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL ?? "",
+        }
+      ).then((res) => {
+        new AuthCookie().signInCallbackCookies({
+          accessToken: res.data.accessToken
+        })
+
+        console.log('new cookie set done')
+      })
+    }
+    console.log('original api request!')
+    return config;
+  }
+)
+
+const isTokenExpired = (): boolean => {
+  const token = new AuthCookie().backendAccessCookie.getToken()
+  if (token !== null) {
+    if (isJwtTokenExpired(token)) return true
+    return false
+  }
+  return true
+}
--- src/common/api/msBackend/auth/getReissueToken.ts
+++
@@ -1,9 +0,0 @@
-import { MS_BACKEND_API_PATH } from "src/constants/msbackend";
-import { MsBackendAxios } from "../../axiosUtil";
-
-interface LoginResponse {
-  accessToken: string;
-}
-
-export const MsApiReissueToken = () =>
-  MsBackendAxios.post<LoginResponse>(MS_BACKEND_API_PATH.POST_REISSUE_TOKEN);
